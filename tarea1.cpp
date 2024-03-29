371dd696e184dada9c57636428e1275416facb31
Git-InE <briosl@usm.cl>
2024-03-29 16:42:53 -0300

    probando

---
+++ tarea1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+struct Pieza {
+    char P; 
+    int x, y;
+};
+// transformar lineas en arrays y utilizar el index con .find//
+struct Tablero {
+    Pieza* piezas_tablero;
+};
+bool tableroEnJaqueMate(Tablero &tablero){
+
+};
