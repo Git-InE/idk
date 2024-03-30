e2c5449d6a87df08b67aa4ff2be8dc782ffcee2d
Git-InE <briosl@usm.cl>
2024-03-29 22:22:28 -0300

    Update tarea1.cpp
    agregando un par de cositas

--- tarea1.cpp
+++ tarea1.cpp
@@ -2,13 +2,21 @@
 #include <iostream>
 using namespace std;
 struct Pieza {
-    char P; 
+    char simbolo; 
     int x, y;
 };
 // transformar lineas en arrays y utilizar el index con .find//
 struct Tablero {
-    Pieza* piezas_tablero;
+    Pieza* piezas_tablero[8][8];
 };
-bool tableroEnJaqueMate(Tablero &tablero){
 
-};
+int main(){
+    int tablero[8][8];
+    for (int i = 0; i < 8; i++){
+        for (int j= 0; j<8; j++){
+            tablero[j] = j;
+            cout << tablero;
+        }
+    }
+    return 0;
+}
