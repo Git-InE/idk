f0746cdb4589ac92b15c49f1d6083e9b9b03c4dc
Git-InE <briosl@usm.cl>
2024-04-06 16:52:07 -0300

    git commit -m

--- tarea1.cpp
+++ tarea1.cpp
@@ -6,46 +6,112 @@ struct Pieza {
     char simbolo; 
     int x, y;
 };
-// transformar lineas en arrays y utilizar el index con .find//
+
 struct Tablero {
-    Pieza* piezas_tablero[8][8];
+    Pieza *piezas_tablero;
 };
-bool tableroEnJaqueMate(Tablero &tablero){
-    bool amenaza;
-    bool jaque;
-    Pieza Rey;
-    char simbolo;
-    for(int i=0; i<8; i++){
-        for(int j=0; j<8; j++){
-            if (tablero.piezas_tablero[i][j]->simbolo == 'X'){
-            Rey = {'X', i, j};
-            }
-        }
-    }
+struct PosRey
+{
+	int x, y;
 };
-int main(){
-    ifstream archivo;
-     std::ifstream archivo("tablero.txt");
-    if (!archivo.is_open()) {
+
+
+// Funciones
+/*****
+ * 
+ * Funcion: ArregloPosRey
+ * 
+ * Resumen Funcion: Funcion que crea un arreglo de posiciones de rey + las 8 posiciones adyacentes
+ * 
+ * Input: 
+ * int indice : indice de la pieza rey en el arreglo de piezas
+ * Tablero tablero : tablero con las piezas
+ * 
+ * Returns:
+ * PosRey*, arreglo de posiciones de rey
+*****/
+PosRey *ArregloPosRey(int indice, Tablero tablero)
+{
+	PosRey *posRey = new PosRey[9];
+	int contador = 0;
+	for (int i = -1; i <= 1; i++)
+	{
+		for (int j = -1; j <= 1; j++)
+		{
+			if ((posRey[contador].y = tablero.piezas_tablero[indice].y + i) > 8 || (posRey[contador].y = tablero.piezas_tablero[indice].y + i) < 0 
+			|| (posRey[contador].y = tablero.piezas_tablero[indice].y + j) > 8 || (posRey[contador].y = tablero.piezas_tablero[indice].y + j) < 0)
+			{
+				posRey[contador].x = -1;
+				posRey[contador].y = -1;
+			}
+			else
+			{
+				if (i == 0 && j == 0)
+				{
+					posRey[contador].x = tablero.piezas_tablero[indice].x;
+					posRey[contador].y = tablero.piezas_tablero[indice].y;
+					cout << "Posicion " << contador << " X: " << posRey[contador].x << " Y: " << posRey[contador].y << endl;
+				}
+				else
+				{
+
+				posRey[contador].x = tablero.piezas_tablero[indice].x + i;
+				posRey[contador].y = tablero.piezas_tablero[indice].y + j;
+				cout << "Posicion " << contador << " X: " << posRey[contador].x << " Y: " << posRey[contador].y << endl;
+				}
+			}
+			contador++;
+		}
+	}
+	return posRey;
+}
+bool tableroEnJaqueMate(Tablero tablero){
+    Tablero mesa;
+    int contador;
+    
+}
+int main()
+{
+	int cantidadPiezas;
+	int posY = 0;
+	int indice = 0;
+	fstream tab;
+	string linea;
+	Tablero tablero;        /*declarar variables*/
+
+	tab.open("tablero.txt", ios::in);
+
+	tab >> cantidadPiezas;
+    
+	tablero.piezas_tablero = new Pieza[cantidadPiezas];
+    if (!tab.is_open()) {       /*checkear si el archivo abrio*/
         std::cerr << "Error al abrir el archivo." << std::endl;
         return 1;
     }
-    Tablero tablero;
-    int cantidad_piezas; /*obtener primera linea del .txt*/
-    archivo >> cantidad_piezas;
-    /*asignar cada letra o punto a una posicion dentro del tablero y sus coordenadas*/
-    for(int i=0; i<cantidad_piezas; i++){
-        char simbolo;
-        int x, y;
-        for(int j=0; j<8; j++){
-            archivo >> simbolo >> x >> y;
-            tablero.piezas_tablero[i][j]->simbolo = simbolo;
-            tablero.piezas_tablero[i][j]->x = i;
-            tablero.piezas_tablero[i][j]->y = j;
+
+	while (getline(tab, linea)) /*guardar piezas en tablero*/
+	{
+		for (int i = 0; i < linea.size(); i++)
+		{
+			if (linea[i] != '.')
+			{
+				tablero.piezas_tablero[indice].simbolo = linea[i];
+				tablero.piezas_tablero[indice].x = i + 1;
+				tablero.piezas_tablero[indice].y = posY;
+			if (linea[i] == 'X')
+			{
+				ArregloPosRey(indice, tablero);
+			}
+			indice++;
+    		}
+    	}
+    	posY++;
     }
-    /*llamar funcion tablerojaquemate*/
+	posY = 0;
+    tab.close();
     bool jaque_mate = tableroEnJaqueMate(tablero);
     tableroEnJaqueMate ? cout << "si" << endl : cout << "no" << endl;
     return 0;
+	delete[] tablero.piezas_tablero;
 }
-};
+
