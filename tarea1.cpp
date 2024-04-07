6fe4d865ce2b99539df69c2f6eaac8a9b2b4468b
Git-InE <briosl@usm.cl>
2024-04-07 00:45:46 -0400

    ahora siii

--- tarea1.cpp
+++ tarea1.cpp
@@ -2,13 +2,16 @@
 #include <string>
 #include <fstream>
 using namespace std;
+
 struct Pieza {
     char simbolo; 
     int x, y;
 };
+
 struct Tablero {
     Pieza *piezas_tablero;
 };
+
 char BuscarPieza(int cantidadPiezas, Tablero tablero, int x, int y){
 	for (int i = 0; i < cantidadPiezas; i++){
 		if (tablero.piezas_tablero[i].x == x && tablero.piezas_tablero[i].y == y){
@@ -17,12 +20,14 @@ char BuscarPieza(int cantidadPiezas, Tablero tablero, int x, int y){
 	}
 	return '.';
 }
-Tablero expandirRey(int n, Tablero tab){
 
+Tablero expandirRey(int n, Tablero tab, int reyindice){
 	Pieza* posRey = new Pieza[9];
-	int i, j, indice = 0;
+	int i, j = 0;
+	int indice = reyindice;
 	int posReyX = tab.piezas_tablero[indice].x;
-	int posReyY = tab.piezas_tablero[indice].y	;
+	int posReyY = tab.piezas_tablero[indice].y;
+	cout << posReyX << ',' << posReyY << endl;
 	int adjX,adjY;
 	int contador = 0;
 	for (int i = -1; i <= 1; i++)
@@ -36,6 +41,7 @@ Tablero expandirRey(int n, Tablero tab){
 				posRey[contador].x = adjX;
 				posRey[contador].y = adjY;
 				posRey[contador].simbolo = BuscarPieza(n, tab, adjX, adjY);
+				cout << adjX << ' '<< adjY << ' ' << posRey[contador].simbolo << endl;
 				contador++;
 			}
 
@@ -44,10 +50,13 @@ Tablero expandirRey(int n, Tablero tab){
 	}
 	return tab;
 }
+
 bool tableroEnJaqueMate(Tablero tablero){
     int contador;
+	/*terminar*/
     return true;
 	}
+
 int main(){
     ifstream archivo;
 	archivo.open("tablero.txt", ios::in);
@@ -58,7 +67,7 @@ int main(){
 	Tablero tablero;
 	char simbolo;
     int cantidad_piezas;
-	int i, j, indice = 0;
+	int i, j, indice, reyindice = 0;
 	string linea;
 	archivo >> cantidad_piezas;
 	tablero.piezas_tablero = new Pieza[cantidad_piezas];
@@ -67,6 +76,9 @@ int main(){
 		for (i=0; i<8; i++){
 			simbolo = linea[i];
 			if (simbolo != '.'){
+				if(simbolo == 'X'){
+					reyindice = indice;
+				}
 				tablero.piezas_tablero[indice].simbolo = simbolo;
 				tablero.piezas_tablero[indice].x = i;
 				tablero.piezas_tablero[indice].y = j;
@@ -75,7 +87,7 @@ int main(){
 		}
 	}
 	Tablero *tab = &tablero;
-	expandirRey(cantidad_piezas, *tab);
+	expandirRey(cantidad_piezas, *tab, reyindice);
 	/*solo pa comprobar*/
 	for (int k=0; k < cantidad_piezas; k++){
 		cout << tablero.piezas_tablero[k].simbolo << ':' << tablero.piezas_tablero[k].x << ',' << tablero.piezas_tablero[k].y << endl;
