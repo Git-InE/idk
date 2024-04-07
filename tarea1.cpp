415e34592cd98d5603790421593f84fe03db6d36
Git-InE <briosl@usm.cl>
2024-04-07 00:24:15 -0400

    mezcle lo tuyo con lo mio, ahora ya no da error

--- tarea1.cpp
+++ tarea1.cpp
@@ -6,112 +6,84 @@ struct Pieza {
     char simbolo; 
     int x, y;
 };
-
 struct Tablero {
     Pieza *piezas_tablero;
 };
-struct PosRey
-{
-	int x, y;
-};
+char BuscarPieza(int cantidadPiezas, Tablero tablero, int x, int y){
+	for (int i = 0; i < cantidadPiezas; i++){
+		if (tablero.piezas_tablero[i].x == x && tablero.piezas_tablero[i].y == y){
+			return tablero.piezas_tablero[i].simbolo;
+		}
+	}
+	return '.';
+}
+Tablero expandirRey(int n, Tablero tab){
 
-
-// Funciones
-/*****
- * 
- * Funcion: ArregloPosRey
- * 
- * Resumen Funcion: Funcion que crea un arreglo de posiciones de rey + las 8 posiciones adyacentes
- * 
- * Input: 
- * int indice : indice de la pieza rey en el arreglo de piezas
- * Tablero tablero : tablero con las piezas
- * 
- * Returns:
- * PosRey*, arreglo de posiciones de rey
-*****/
-PosRey *ArregloPosRey(int indice, Tablero tablero)
-{
-	PosRey *posRey = new PosRey[9];
+	Pieza* posRey = new Pieza[9];
+	int i, j, indice = 0;
+	int posReyX = tab.piezas_tablero[indice].x;
+	int posReyY = tab.piezas_tablero[indice].y	;
+	int adjX,adjY;
 	int contador = 0;
 	for (int i = -1; i <= 1; i++)
 	{
 		for (int j = -1; j <= 1; j++)
 		{
-			if ((posRey[contador].y = tablero.piezas_tablero[indice].y + i) > 8 || (posRey[contador].y = tablero.piezas_tablero[indice].y + i) < 0 
-			|| (posRey[contador].y = tablero.piezas_tablero[indice].y + j) > 8 || (posRey[contador].y = tablero.piezas_tablero[indice].y + j) < 0)
+			adjX = posReyX + i;
+			adjY = posReyY + j;
+			if (adjX >= 0 && adjX < 8 && adjY >= 0 && adjY < 8)
 			{
-				posRey[contador].x = -1;
-				posRey[contador].y = -1;
+				posRey[contador].x = adjX;
+				posRey[contador].y = adjY;
+				posRey[contador].simbolo = BuscarPieza(n, tab, adjX, adjY);
+				contador++;
 			}
-			else
-			{
-				if (i == 0 && j == 0)
-				{
-					posRey[contador].x = tablero.piezas_tablero[indice].x;
-					posRey[contador].y = tablero.piezas_tablero[indice].y;
-					cout << "Posicion " << contador << " X: " << posRey[contador].x << " Y: " << posRey[contador].y << endl;
-				}
-				else
-				{
 
-				posRey[contador].x = tablero.piezas_tablero[indice].x + i;
-				posRey[contador].y = tablero.piezas_tablero[indice].y + j;
-				cout << "Posicion " << contador << " X: " << posRey[contador].x << " Y: " << posRey[contador].y << endl;
-				}
-			}
-			contador++;
+			
 		}
 	}
-	return posRey;
+	return tab;
 }
 bool tableroEnJaqueMate(Tablero tablero){
-    Tablero mesa;
     int contador;
-    
-}
-int main()
-{
-	int cantidadPiezas;
-	int posY = 0;
-	int indice = 0;
-	fstream tab;
-	string linea;
-	Tablero tablero;        /*declarar variables*/
-
-	tab.open("tablero.txt", ios::in);
-
-	tab >> cantidadPiezas;
-    
-	tablero.piezas_tablero = new Pieza[cantidadPiezas];
-    if (!tab.is_open()) {       /*checkear si el archivo abrio*/
+    return true;
+	}
+int main(){
+    ifstream archivo;
+	archivo.open("tablero.txt", ios::in);
+    if (!archivo.is_open()) {
         std::cerr << "Error al abrir el archivo." << std::endl;
         return 1;
-    }
-
-	while (getline(tab, linea)) /*guardar piezas en tablero*/
-	{
-		for (int i = 0; i < linea.size(); i++)
-		{
-			if (linea[i] != '.')
-			{
-				tablero.piezas_tablero[indice].simbolo = linea[i];
-				tablero.piezas_tablero[indice].x = i + 1;
-				tablero.piezas_tablero[indice].y = posY;
-			if (linea[i] == 'X')
-			{
-				ArregloPosRey(indice, tablero);
+	}
+	Tablero tablero;
+	char simbolo;
+    int cantidad_piezas;
+	int i, j, indice = 0;
+	string linea;
+	archivo >> cantidad_piezas;
+	tablero.piezas_tablero = new Pieza[cantidad_piezas];
+	for (j=0; j<8; j++){
+		archivo >> linea;
+		for (i=0; i<8; i++){
+			simbolo = linea[i];
+			if (simbolo != '.'){
+				tablero.piezas_tablero[indice].simbolo = simbolo;
+				tablero.piezas_tablero[indice].x = i;
+				tablero.piezas_tablero[indice].y = j;
+				indice++;
 			}
-			indice++;
-    		}
-    	}
-    	posY++;
-    }
-	posY = 0;
-    tab.close();
+		}
+	}
+	Tablero *tab = &tablero;
+	expandirRey(cantidad_piezas, *tab);
+	/*solo pa comprobar*/
+	for (int k=0; k < cantidad_piezas; k++){
+		cout << tablero.piezas_tablero[k].simbolo << ':' << tablero.piezas_tablero[k].x << ',' << tablero.piezas_tablero[k].y << endl;
+	}
     bool jaque_mate = tableroEnJaqueMate(tablero);
     tableroEnJaqueMate ? cout << "si" << endl : cout << "no" << endl;
-    return 0;
 	delete[] tablero.piezas_tablero;
-}
+	archivo.close();
+    return 0;
+};
 
