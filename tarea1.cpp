0db35f21f5d357612974c9e4c6e4ad03df0c0a2d
Git-InE <165421006+Git-InE@users.noreply.github.com>
2024-04-13 14:47:44 -0400

    probando probando

--- tarea1.cpp
+++ tarea1.cpp
@@ -1,26 +1,8 @@
-// Importante: utilizar este tipo de comentario para las funciones, si no quitan 5 pts 
-/*****
-* TipoFunción NombreFunción
-******
-* Resumen Función
-******
-* Input:
-* tipoParámetro NombreParámetro : Descripción Parámetro
-* .......
-******
-* Returns:
-* TipoRetorno, Descripción retorno
-*****/
-
 #include <iostream>
 #include <string>
 #include <fstream>
 using namespace std;
-struct PiezaRey {
-    char simbolo; 
-    int x, y;
-	bool amenaza;
-};
+
 struct Pieza {
     char simbolo; 
     int x, y;
@@ -30,10 +12,33 @@ struct Tablero {
 	int cantidad_piezas;
 	Pieza *piezas_tablero;
 };
+
+struct PiezaRey {
+    int x, y;
+    char simbolo; 
+	bool amenaza;
+};
+
 PiezaRey arreglorey[9];
 int reyindice, espacios_validos = 0;
 
-char BuscarPieza(Tablero tablero, int x, int y){
+/*****
+* char BuscarPieza
+******
+* Resumen Función
+*
+******
+* Input:
+* Tablero tablero, tablero con las piezas
+* int x, coordenada x de la pieza
+* int y, coordenada y de la pieza
+******
+* Returns:
+* char, simbolo de la pieza en la posicion x, y
+* si no hay pieza en esa posicion retorna '.'
+*****/
+
+char BuscarPieza( Tablero tablero, int x, int y){
 	for (int i = 0; i < tablero.cantidad_piezas; i++){
 		if (tablero.piezas_tablero[i].x == x && tablero.piezas_tablero[i].y == y){
 			return tablero.piezas_tablero[i].simbolo;
@@ -42,7 +47,22 @@ char BuscarPieza(Tablero tablero, int x, int y){
 	return '.';
 }
 
-bool jaquePeon(Tablero tablero)
+/*****
+* bool jaquePeon
+******
+* Resumen Función
+* Primero busca en el arreglo de piezas si hay un peon existente,
+* en caso de que exista, busca si el rey está en jaque desde la posición del peon
+* dado que el peon solo puede atacar en diagonal con 1 "unidad", se busca si el rey está en diagonal de 1 "unidad"
+******
+* Input:
+* Tablero tablero, tablero con las piezas
+******
+* Returns:
+* bool, true si el rey está en jaque, false si no lo está
+*****/
+
+void jaquePeon(Tablero tablero)
 {
 	int indice = 0;
 	for (indice; indice < espacios_validos; indice++)
@@ -65,155 +85,223 @@ bool jaquePeon(Tablero tablero)
 			}
 
 	}
-    return false;
+    return;
 }
+/*****
+* void jaqueAlfil
+******
+* Resumen Función
+* Crea dos arreglos, uno para las piezas adyacentes y otro para las posiciones con un "."
+* despues de eso inicia un ciclo for, en el cual guarda las piezas adyacentes en el arreglo adj
+* si la pieza adyacente es un alfil y la cantidad de posiciones vacias es igual a i-1, entonces el rey está en jaque
+* en caso de que no se cumpla, agregará 1 al contador de piezas adyacentes y si la pieza adyacente es distinta de "."
+* entonces agregará 1 al contador de piezas adyacentes
+* si la cantidad de piezas adyacentes es igual a 4, entonces retornará false
+******
+* Input:
+* Tablero tablero, tablero con las piezas
+******
+* Returns:
+* void, true si el rey está en jaque, false si no lo está
+*****/
 
-bool jaqueTorre(Tablero& tablero){
-	int indice = 0;
-	for (indice; indice < espacios_validos; indice++)
-	{
-		int posXRey = arreglorey[indice].x;
-    	int posYRey = arreglorey[indice].y;
-		for (int x = posXRey + 1; x < 8; ++x) 
-		{
-            char pieza = BuscarPieza(tablero, x, posYRey);
-            if (pieza == 'T') {
-				arreglorey[indice].amenaza = true;
-            } else if (pieza != '.') {
-                break; // Pieza diferente, detener búsqueda (colision)
+void jaqueAlfil(Tablero tablero){
+
+    int indice = 0;
+    for (indice; indice < espacios_validos; indice++)
+    {
+        int posXRey = arreglorey[indice].x;
+        int posYRey = arreglorey[indice].y;
+        char adj[4];
+        int vacios[4] = {0, 0, 0, 0};
+        int numOtras;
+        for (int i = 1; i < 8; i++)
+        {
+            numOtras = 0;
+            adj[0] = BuscarPieza(tablero, posXRey + i, posYRey + i);
+            adj[1] = BuscarPieza(tablero, posXRey + i, posYRey - i);
+            adj[2] = BuscarPieza(tablero, posXRey - i, posYRey + i);
+            adj[3] = BuscarPieza(tablero, posXRey - i, posYRey - i);
+
+            for (int j = 0; j < 4; j++)
+            {
+                if ((adj[j] == 'A')&&(vacios[j]== i - 1)){
+                    cout << "Jaque por Alfil" << endl;
+                    arreglorey[indice].amenaza = true;
+                } 
+                if (adj[j] != '.') numOtras++;
+                if (adj[j] == '.') vacios[j]++;
             }
         }
+    return;
+    }
+}
+/*****
+* void jaqueReina
+******
+* Resumen Función
+* Esta funcion es una mezcla de las funciones jaqueAlfil y jaqueTorre
+* la cual basicamente crea 4 arreglos 2 "horizontales" y 2 "verticales",
+* en los cuales se guardan las piezas adyacentes y las posiciones vacias, 1 arreglo para cada tipo de movimiento de la reina
+* despues de eso inicia un ciclo for, en el cual guarda las piezas adyacentes en el arreglo adj
+* si la pieza adyacente es un reina y la cantidad de posiciones vacias es igual a i-1, entonces el rey está en jaque
+* en caso de que no se cumpla, agregará 1 al contador de piezas adyacentes y si la pieza adyacente es distinta de "."
+* entonces agregará 1 al contador de piezas adyacentes
+* si la cantidad de piezas adyacentes es igual a 4, entonces retornará false
+******
+* Input:
+* Tablero tablero, tablero con las piezas
+******
+* Returns:
+* void, true si el rey está en jaque, false si no lo está
+*****/
+void jaqueReina(Tablero tablero){
+    int indice = 0;
+    int jaqueX, jaqueY;
 
-		for (int x = posXRey - 1; x >= 0; x--)
-		{
-			char pieza = BuscarPieza(tablero, x, posYRey);
-            if (pieza == 'T') {
-				arreglorey[indice].amenaza = true;
-            } else if (pieza != '.') {
-                break;
+    for (indice; indice < espacios_validos; indice++)
+    {        
+        int posXRey = arreglorey[indice].x;
+        int posYRey = arreglorey[indice].y;
+	    char adjT[4];
+        int vaciosT[4] = {0, 0, 0, 0};
+        int numOtrasT;
+        for (int i = 1; i < 8; i++)
+        {
+            numOtrasT = 0;
+            adjT[0] = BuscarPieza(tablero, posXRey + i, posYRey);
+            adjT[1] = BuscarPieza(tablero, posXRey - i, posYRey);
+            adjT[2] = BuscarPieza(tablero, posXRey, posYRey + i);
+            adjT[3] = BuscarPieza(tablero, posXRey, posYRey - i);
+
+            for (int j = 0; j < 4; j++)
+            {
+                if ((adjT[j] == 'R')&&(vaciosT[j] == i - 1)){
+                    cout << "Jaque por Reina" << endl;
+                    arreglorey[indice].amenaza = true;
+                } 
+                if (adjT[j] != '.') numOtrasT++;
+                if (adjT[j] == '.') vaciosT[j]++;
             }
-		}
+        }
+        char adjA[4];
+        int vaciosA[4] = {0, 0, 0, 0};
+        int numOtrasA;
+        for (int i = 1; i < 8; i++)
+        {
+            numOtrasA = 0;
+            adjA[0] = BuscarPieza(tablero, posXRey + i, posYRey + i);
+            adjA[1] = BuscarPieza(tablero, posXRey + i, posYRey - i);
+            adjA[2] = BuscarPieza(tablero, posXRey - i, posYRey + i);
+            adjA[3] = BuscarPieza(tablero, posXRey - i, posYRey - i);
 
-		for (int y = posYRey + 1; y < 8 - posYRey; y++)
-		{
-			char pieza = BuscarPieza(tablero, posXRey, y);
-            if (pieza == 'T') {
-				arreglorey[indice].amenaza = true;
-            } else if (pieza != '.') {
-                break;
+            for (int j = 0; j < 4; j++)
+            {
+                if ((adjA[j] == 'R')&&(vaciosA[j] == i - 1)){
+                    cout << "Jaque por Reina" << endl;
+                    arreglorey[indice].amenaza = true;
+                } 
+                if (adjA[j] != '.') numOtrasA++;
+                if (adjA[j] == '.') vaciosA[j]++;
             }
-		}
+        }
+    }
+    return;
+}
 
-		for (int y = posYRey - 1; y >= 0; y--)
-		{
-			char pieza = BuscarPieza(tablero, posXRey, y);
-            if (pieza == 'T') {
-				arreglorey[indice].amenaza = true;
-            } else if (pieza != '.') {
-                break;
+/*****
+* void jaqueTorre
+******
+* Resumen Función
+* Crea dos arreglos, uno para las piezas adyacentes y otro para las posiciones con un "."
+* despues de eso inicia un ciclo for, en el cual guarda las piezas adyacentes en el arreglo adj
+* si la pieza adyacente es un torre y la cantidad de posiciones vacias es igual a i-1, entonces el rey está en jaque
+* en caso de que no se cumpla, agregará 1 al contador de piezas adyacentes y si la pieza adyacente es distinta de "."
+* entonces agregará 1 al contador de piezas adyacentes
+******
+* Input:
+* Tablero tablero, tablero con las piezas
+******
+* Returns:
+* void, true si el rey está en jaque, false si no lo está
+*****/
+
+void jaqueTorre(Tablero& tablero){
+	int indice = 0;
+    for (indice; indice < espacios_validos; indice++)
+    {
+        int posXRey = arreglorey[indice].x;
+    	int posYRey = arreglorey[indice].y;
+        char adj[4];
+        int vacios[4] = {0, 0, 0, 0};
+        int numOtras;
+        for (int i = 1; i < 8; i++)
+        {
+            numOtras = 0;
+            adj[0] = BuscarPieza(tablero, posXRey + i, posYRey);
+            adj[1] = BuscarPieza(tablero, posXRey - i, posYRey);
+            adj[2] = BuscarPieza(tablero, posXRey, posYRey + i);
+            adj[3] = BuscarPieza(tablero, posXRey, posYRey - i);
+            for (int j = 0; j < 4; j++)
+            {
+                if ((adj[j] == 'T') && (vacios[j] == i - 1)){
+                    arreglorey[indice].amenaza = true;
+                } 
+                if (adj[j] != '.') numOtras++;
+                if (adj[j] == '.') vacios[j]++;
             }
-		}
-	}
-	return false;
+
+
+        }
+    }
+    return;
 }
 
-bool jaqueCaballo(Tablero tablero){
-	int posXRey = tablero.piezas_tablero[reyindice].x;
-	int posYRey = tablero.piezas_tablero[reyindice].y;
-	
-	if (BuscarPieza(tablero, posXRey + 1, posYRey + 2) == 'C')
-	{
-		cout << "Jaque" << endl;
-		return true;
-	}
-	else if (BuscarPieza(tablero, posXRey + 1, posYRey - 2) == 'C')
-	{
-		cout << "Jaque" << endl;
-		return true;
-	}
-	else if (BuscarPieza(tablero, posXRey - 1, posYRey + 2) == 'C')
-	{
-		cout << "Jaque" << endl;
-		return true;
-	}
-	else if (BuscarPieza(tablero, posXRey - 1, posYRey - 2) == 'C')
-	{
-		cout << "Jaque" << endl;
-		return true;
-	}
-	else if (BuscarPieza(tablero, posXRey + 2, posYRey + 1) == 'C')
-	{
-		cout << "Jaque" << endl;
-		return true;
-	}
-	else if (BuscarPieza(tablero, posXRey + 2, posYRey - 1) == 'C')
-	{
-		cout << "Jaque" << endl;
-		return true;
-	}
-	else if (BuscarPieza(tablero, posXRey - 2, posYRey + 1) == 'C')
-	{
-		cout << "Jaque" << endl;
-		return true;
-	}
-	else if (BuscarPieza(tablero, posXRey - 2, posYRey - 1) == 'C')
-	{
-		cout << "Jaque" << endl;
-		return true;
-	}
-	return false;
+/*****
+* void jaqueCaballo
+******
+* Resumen Función
+* utilizando la posicion del rey, se busca si existe un caballo en las 8 posibles posiciones
+* en caso de que exista un caballo en alguna de las posiciones, entonces el rey está en jaque
+* en este caso no se necesita buscar si hay una pieza entre el rey y el caballo, ya que el caballo puede saltar 
+* por las reglas del ajedrez
+******
+* Input:
+* Tablero tablero, tablero con las piezas
+******
+* Returns:
+* void, true si el rey está en jaque, false si no lo está
+*****/
+
+void jaqueCaballo(Tablero tablero){
+	int indice = 0;
+    for (indice; indice < espacios_validos; indice++)
+    {
+        int posXRey = arreglorey[indice].x;
+    	int posYRey = arreglorey[indice].y;
+        for (int i = 0; i < tablero.cantidad_piezas; i++)
+        {
+            if (tablero.piezas_tablero[i].simbolo == 'C')
+            {
+                if ((tablero.piezas_tablero[i].x == posXRey + 2 && tablero.piezas_tablero[i].y == posYRey + 1) ||
+                    (tablero.piezas_tablero[i].x == posXRey + 2 && tablero.piezas_tablero[i].y == posYRey - 1) ||
+                    (tablero.piezas_tablero[i].x == posXRey - 2 && tablero.piezas_tablero[i].y == posYRey + 1) ||
+                    (tablero.piezas_tablero[i].x == posXRey - 2 && tablero.piezas_tablero[i].y == posYRey - 1) ||
+                    (tablero.piezas_tablero[i].x == posXRey + 1 && tablero.piezas_tablero[i].y == posYRey + 2) ||
+                    (tablero.piezas_tablero[i].x == posXRey + 1 && tablero.piezas_tablero[i].y == posYRey - 2) ||
+                    (tablero.piezas_tablero[i].x == posXRey - 1 && tablero.piezas_tablero[i].y == posYRey + 2) ||
+                    (tablero.piezas_tablero[i].x == posXRey - 1 && tablero.piezas_tablero[i].y == posYRey - 2))
+                {
+                    cout << "Jaque por Caballo" << endl;
+                    arreglorey[indice].amenaza = true;
+                }
+            }
+        }
+    }
+    return;
 }
 
-bool jaqueAlfil(Tablero tablero){
-	int posXRey = tablero.piezas_tablero[reyindice].x;
-	int posYRey = tablero.piezas_tablero[reyindice].y;
-	
-	for (int RT = 1; RT < 8; RT++){
-		if (BuscarPieza(tablero, posXRey + RT, posYRey + RT) == 'A'){
-			cout << "Jaque" << endl;
-			return true;
-		}
-	}
-	for (int LT = 1; LT < 8; LT++){
-		if (BuscarPieza(tablero, posXRey - LT, posYRey + LT) == 'A'){
-			cout << "Jaque" << endl;
-			return true;
-		}
-	}
-	for (int RB = 1; RB < 8; RB++){
-		if (BuscarPieza(tablero, posXRey + RB, posYRey - RB) == 'A'){
-			cout << "Jaque" << endl;
-			return true;
-		}
-	}
-	for (int LB = 1; LB < 8; LB++){
-		if (BuscarPieza(tablero, posXRey - LB, posYRey - LB) == 'A'){
-			cout << "Jaque" << endl;
-			return true;
-		}
-	}
-	return false;
-}
-
-bool tableroEnJaqueMate(Tablero tablero){
-    int contador;
-	/*terminar*/
-    return true;
-	}
-// No se si funciona porque falta la funcion jaqueAlfil
-bool jaqueReina(Tablero tablero){
-	int posXRey = tablero.piezas_tablero[reyindice].x;
-	int posYRey = tablero.piezas_tablero[reyindice].y;
-
-	if (jaqueTorre(tablero) || jaqueAlfil(tablero))
-	{
-		cout << "Jaque" << endl;
-		return true;
-	}
-	return false;	
-}
-
-int expandirRey(Tablero tab){
+void expandirRey(Tablero tab){
 	int indice = reyindice;
 	int posReyX = tab.piezas_tablero[indice].x;
 	int posReyY = tab.piezas_tablero[indice].y;
@@ -237,9 +325,29 @@ int expandirRey(Tablero tab){
 	}
 	espacios_validos = contador;
 
-	return espacios_validos;
+	return;
 }
 
+bool tableroEnJaqueMate(Tablero tablero){
+    int contador=0;
+    jaquePeon(tablero);
+	jaqueTorre(tablero);
+	jaqueCaballo(tablero);
+	jaqueAlfil(tablero);
+    jaqueReina(tablero);
+    for (int i=0; i < espacios_validos; i++)
+    {
+        if (arreglorey[i].amenaza){
+            contador++;
+        }
+    }
+    if (contador == espacios_validos){
+        return true;
+    }
+    else{
+        return false;
+    }
+	}
 
 int main(){
     ifstream archivo;
@@ -253,6 +361,7 @@ int main(){
 	int i, j;
 	int  indice = 0;
 	string linea;
+    cout << "hola" << endl;
 	archivo >> tablero.cantidad_piezas;
 	tablero.piezas_tablero = new Pieza[tablero.cantidad_piezas];
 	for (j=0; j<8; j++){
@@ -271,17 +380,9 @@ int main(){
 		}
 	}
 	Tablero *tab = &tablero;
-	int validos = expandirRey(*tab);
-	
-	// solo para ver si funciona las funciones
-	bool jaque_peon = jaquePeon(*tab);
-	bool jaque_torre = jaqueTorre(*tab);
-	bool jaque_caballo = jaqueCaballo(*tab);
-	bool jaque_alfil = jaqueAlfil(*tab);
-
-
+	expandirRey(*tab);
     bool jaque_mate = tableroEnJaqueMate(tablero);
-    tableroEnJaqueMate ? cout << "si" << endl : cout << "no" << endl;
+    jaque_mate ? cout << "si" << endl : cout << "no" << endl;
 	delete[] tablero.piezas_tablero;
 	archivo.close();
     return 0;
